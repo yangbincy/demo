a62c6734e431b66f132ec3e3f4c3c1c976b8177e
yangbincy <50374215+yangbincy@users.noreply.github.com>
2019-05-20 17:15:56 +0800

    Update DemoApplication.java

--- src/main/java/com/offcn/DemoApplication.java
+++ src/main/java/com/offcn/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+    //github测试
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
