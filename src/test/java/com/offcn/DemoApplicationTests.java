1a79e6f8df3d46741e7938b86c312c324f0dd785
yangb <yangbinwinner@163.com>
2019-05-20 17:09:59 +0800

    测试git上传

---
+++ src/test/java/com/offcn/DemoApplicationTests.java
@@ -0,0 +1,21 @@
+package com.offcn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+    @Autowired
+
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
